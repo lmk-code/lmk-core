062104fdfb729ccfd60b5c4b262b4f5cfe67fab1
laomake <laomake@hotmail.com>
2021-02-10 21:45:40 +0800

    优化AdminEntity

--- core-web/src/main/java/com/lmk/core/boot/entity/AdminEntity.java
+++ core-web/src/main/java/com/lmk/core/boot/entity/AdminEntity.java
@@ -2,6 +2,8 @@ package com.lmk.core.boot.entity;
 
 import com.lmk.core.web.auth.bean.LoginUser;
 
+import java.util.Date;
+
 /**
  * 管理后台实体
  * @author 编程浪子
@@ -12,11 +14,33 @@ public class AdminEntity extends IdEntity{
     /** 当前登录用户 */
     protected LoginUser loginUser;
 
+    /** 创建时间 */
+    protected Date createTime;
+
+    /** 修改时间 */
+    protected Date updateTime;
+
     public LoginUser getLoginUser() {
         return loginUser;
     }
 
     public void setLoginUser(LoginUser loginUser) {
         this.loginUser = loginUser;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
     }
 }
